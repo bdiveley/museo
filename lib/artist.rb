50806f89934b116fbda29b3ac571bed0295e0ad1
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-18 09:15:02 -0600

    Write and test artist class and complete iteration 1

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,16 @@
 class Artist
+  attr_reader :id,
+              :name,
+              :born,
+              :died,
+              :country
 
-end 
+  def initialize(artist_hash)
+    @id = artist_hash[:id]
+    @name = artist_hash[:name]
+    @born = artist_hash[:born]
+    @died = artist_hash[:died]
+    @country = artist_hash[:country]
+  end
+
+end
