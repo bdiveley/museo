1461c0a40809df1f3a6b40bdc93fe7680fbb8835
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-18 09:11:19 -0600

    Create and test photography class

---
+++ lib/photograph.rb
@@ -0,0 +1,14 @@
+class Photograph
+  attr_reader :id,
+              :name,
+              :artist_id,
+              :year
+
+  def initialize(photo_hash)
+    @id = photo_hash[:id]
+    @name = photo_hash[:name]
+    @artist_id = photo_hash[:artist_id]
+    @year = photo_hash[:year]
+  end
+
+end
